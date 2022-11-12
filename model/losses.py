7ee38384cb6df8cfbbc48f09260074585bf26bb0
MadridMaverick <83025525+MadridMaverick@users.noreply.github.com>
2022-11-12 09:56:09 +0700

    added etf

--- model/losses.py
+++ model/losses.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 # Classes
 class FocalLoss(nn.CrossEntropyLoss):
     """ Focal loss for classification tasks on imbalanced datasets """
+
     def __init__(self, gamma, alpha=None, ignore_index=-100, reduction='none'):
         super().__init__(weight=alpha, ignore_index=ignore_index, reduction='none')
         self.reduction = reduction
@@ -25,3 +26,45 @@ class FocalLoss(nn.CrossEntropyLoss):
             return torch.sum(loss)
         else:
             return loss
+
+
+# Functions
+def dot_loss(output, label, cur_M, classifier, criterion, H_length, reg_lam=0):
+    target = cur_M[:, label].T  # B, d  output: B, d
+    if criterion == 'dot_loss':
+        loss = - torch.bmm(output.unsqueeze(1),
+                           target.unsqueeze(2)).view(-1).mean()
+    elif criterion == 'reg_dot_loss':
+        # + classifier.module.bias[label].view(-1)
+        dot = torch.bmm(output.unsqueeze(1), target.unsqueeze(2)).view(-1)
+
+        with torch.no_grad():
+            M_length = torch.sqrt(
+                torch.sum(target ** 2, dim=1, keepdims=False))
+        loss = (1/2) * torch.mean(((dot-(M_length * H_length)) ** 2) / H_length)
+
+        if reg_lam > 0:
+            reg_Eh_l2 = torch.mean(torch.sqrt(
+                torch.sum(output ** 2, dim=1, keepdims=True)))
+            loss = loss + reg_Eh_l2*reg_lam
+
+    return loss
+
+
+def produce_Ew(label, num_classes):
+    uni_label, count = torch.unique(label, return_counts=True)
+    batch_size = label.size(0)
+    uni_label_num = uni_label.size(0)
+
+    assert batch_size == torch.sum(count)
+    gamma = batch_size / uni_label_num
+    Ew = torch.ones(1, num_classes).cuda(label.device)
+    for i in range(uni_label_num):
+        label_id = uni_label[i]
+        label_count = count[i]
+        length = torch.sqrt(gamma / label_count)
+#        length = (gamma / label_count)
+        #length = torch.sqrt(label_count / gamma)
+        Ew[0, label_id] = length
+
+    return Ew
--- model/modules.py
+++ model/modules.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -30,6 +31,44 @@ class CustomReLU(nn.Module):
 
     def forward(self, x):
         return _custom_relu(x)
+
+
+class ETF_Classifier(nn.Module):
+    def __init__(self, feat_in, num_classes, fix_bn=False, LWS=False, reg_ETF=False):
+        super(ETF_Classifier, self).__init__()
+        P = self.generate_random_orthogonal_matrix(feat_in, num_classes)
+        I = torch.eye(num_classes)
+        one = torch.ones(num_classes, num_classes)
+        M = np.sqrt(num_classes / (num_classes-1)) * \
+            torch.matmul(P, I-((1/num_classes) * one))
+        self.ori_M = M.cuda()
+
+        self.LWS = LWS
+        self.reg_ETF = reg_ETF
+#        if LWS:
+#            self.learned_norm = nn.Parameter(torch.ones(1, num_classes))
+#            self.alpha = nn.Parameter(1e-3 * torch.randn(1, num_classes).cuda())
+#            self.learned_norm = (F.softmax(self.alpha, dim=-1) * num_classes)
+#        else:
+#            self.learned_norm = torch.ones(1, num_classes).cuda()
+
+        self.BN_H = nn.BatchNorm1d(feat_in)
+        if fix_bn:
+            self.BN_H.weight.requires_grad = False
+            self.BN_H.bias.requires_grad = False
+
+    def generate_random_orthogonal_matrix(self, feat_in, num_classes):
+        a = np.random.random(size=(feat_in, num_classes))
+        P, _ = np.linalg.qr(a)
+        P = torch.tensor(P).float()
+        assert torch.allclose(torch.matmul(P.T, P), torch.eye(
+            num_classes), atol=1e-07), torch.max(torch.abs(torch.matmul(P.T, P) - torch.eye(num_classes)))
+        return P
+
+    def forward(self, x):
+        x = self.BN_H(x)
+        x = x @ self.ori_M
+        return x
 
 
 # Functions
